e1efc49a4ae80413174c038ad1be077e01a3338a
Víctor Riveros <riverosvictordev@gmail.com>
2024-06-15 01:34:32 -0400

    Se actualiza el archivo conexión para db en deploy.

--- config/conexion.js
+++ config/conexion.js
@@ -5,15 +5,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const config = {
-        host: process.env.DB_HOST,
-        port: process.env.DB_PORT,
-        database: process.env.DB_DATABASE,
-        user: process.env.DB_USER,
-        password: process.env.DB_PASSWORD,
-        max: parseInt(process.env.DB_MAX),
-        min: parseInt(process.env.DB_MIN),
-        idleTimeoutMillis: parseInt(process.env.DB_IDLE_TIMEOUT_MILLIS),
-        connectionTimeoutMillis: parseInt(process.env.DB_CONNECTION_TIMEOUT_MILLIS)
+        connectionString: process.env.DATABASE_URL
 };
 
 const pool = new Pool(config);
